79105bf4f4cdc920425271674b131fc84d0088e6
Radu B <radu.buciuceanu@gmail.com>
2019-04-09 12:52:28 +0300

    person_repository tests were refactored.

--- spec/data/repositories/person_repository_spec.rb
+++ spec/data/repositories/person_repository_spec.rb
@@ -2,49 +2,43 @@ require 'rspec'
 require './src/data/repositories/person_repository'
 require './src/business/models/person'
 
-PersonRepository = Template::Data::Repositories::PersonRepository
-Person = Template::Business::Models::Person
+describe Template::Data::Repositories::PersonRepository do
+    let(:person) {build_person}
 
-describe 'PersonRepository' do
-    before do
-        @instance = PersonRepository.new
-        @existent_person = build_person
-    end
+    subject(:instance) {described_class.new}
 
-    context :insert do
+    context 'insert' do
         it 'returns inserted person' do
-            @instance.insert(Person.new)
+            instance.insert(person)
         end
 
         it 'throws when id already exists' do
-            @instance.insert(@existent_person)
+            instance.insert(person)
 
-            expect{@instance.insert(@existent_person)}.to raise_error ArgumentError, 'Id already exists.'
+            expect {instance.insert(person)}.to raise_error(ArgumentError, 'Id already exists.')
         end
     end
 
-    context :get do
-        before do
-            @instance.insert(@existent_person)
-        end
-
+    context 'get' do
         it 'returns person when id exists' do
-            actual = @instance.get(@existent_person.id)
+            instance.insert(person)
 
-            expect(actual).to equal(@existent_person)
+            actual = instance.get(person.id)
+
+            expect(actual).to equal(person)
         end
 
         it 'throws when id does not exist' do
             nonexistent_id = 'Nonexistent id here.'
 
-            expect {@instance.get(nonexistent_id)}.to raise_error ArgumentError, 'Id does not exist.'
+            expect {instance.get(nonexistent_id)}.to raise_error(ArgumentError, 'Id does not exist.')
         end
     end
 
     private
 
     def build_person
-        person = Person.new
+        person = Template::Business::Models::Person.new
         person.id = 'Random id here.'
         person.first_name = 'Random first name here.'
         person.last_name = 'Random last name here.'
