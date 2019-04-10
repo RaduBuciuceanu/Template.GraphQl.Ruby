4e1d7122c712ba407d5040bfec94a79b965d1373
Radu B <radu.buciuceanu@gmail.com>
2019-04-10 11:38:39 +0300

    New commands were added to Ioc.

--- spec/presentation/ioc/container_spec.rb
+++ spec/presentation/ioc/container_spec.rb
@@ -4,6 +4,18 @@ require './src/presentation/ioc/container'
 describe Template::Presentation::Ioc::Container do
     subject(:instance) {described_class}
 
+    it 'generate_id is registered' do
+        actual = instance[:generate_id]
+
+        expect(actual).to be_instance_of(Template::Business::Commands::GenerateId)
+    end
+
+    it 'person_input_to_person is registered' do
+        actual = instance[:person_input_to_person]
+
+        expect(actual).to be_instance_of(Template::Business::Commands::Persons::PersonInputToPerson)
+    end
+
     it 'create_person is registered' do
         actual = instance[:create_person]
 
--- src/presentation/ioc/register_business_commands.rb
+++ src/presentation/ioc/register_business_commands.rb
@@ -1,11 +1,15 @@
 require './src/business/commands/persons/create_person'
 require './src/business/commands/persons/get_person'
+require './src/business/commands/generate_id'
+require './src/business/commands/persons/person_input_to_person'
 
 module Template
     module Presentation
         module Ioc
             class RegisterBusinessCommands
                 def self.execute(container)
+                    container.register(:generate_id, Business::Commands::GenerateId.new)
+                    container.register(:person_input_to_person, Business::Commands::Persons::PersonInputToPerson.new(container[:generate_id]))
                     container.register(:create_person, Business::Commands::Persons::CreatePerson.new(container[:person_repository]))
                     container.register(:get_person, Business::Commands::Persons::GetPerson.new(container[:person_repository]))
                 end
